8f3aa1a1d17f369829b31ba3811c1d65e5fbc0a3
Radek Golan jr. (golyalpha) <golyalpha@gmail.com>
2018-09-11 07:06:59 +0200

    Fixed the default node_net value being an IPvXNetwork object Until now, the ipaddress.ip_network was being given the IP of the local computer, and made to assume it was a network address. Resulting in a weird IPvXNetwork object. Since IPvXNetwork object is very much useless for us in this instance, creating an IPvXAddress instead.

--- blender_renderfarm/__init__.py
+++ blender_renderfarm/__init__.py
@@ -2,7 +2,7 @@ import requests, socket, ipaddress
 __version__ = '0.1.0'
 
 
-def discover_nodes(node_net=ipaddress.ip_network(socket.gethostbyname(socket.getfqdn()))):
+def discover_nodes(node_net=ipaddress.ip_address(socket.gethostbyname(socket.getfqdn()))):
     print(node_net)
 
 if __name__ == "__main__":
