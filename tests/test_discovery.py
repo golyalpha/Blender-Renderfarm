c63fd12d8e90689517f97139998c80e12bfe2c92
Radek Golan jr. (golyalpha) <golyalpha@gmail.com>
2018-09-18 17:38:43 +0200

    Update tests to reflect new state of the repo

--- tests/test_discovery.py
+++ tests/test_discovery.py
@@ -1,27 +1,30 @@
 import socket
+import os, signal
 from threading import Thread
-
-from blender_renderfarm.server_discovery import (check_port, discover_nodes,
-                                                 get_info)
-
+from blender_renderfarm.server_discovery import (
+    check_port, 
+    discover_nodes,
+    get_info
+)
+from subprocess import Popen, run
+from time import sleep
 
 def test_discovery_empty():
     assert discover_nodes() == []
 
 
-def test_port_check():
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as socket_obj:
-        socket_obj.bind(("localhost", 8080))
-        socket_obj.listen()
-        nodes = []
-        check_port("localhost", 8080, nodes)
-        assert all(nodes)
+def test_discovery_filled():
+    if run("blender --help", capture_output=True):
+        blender = Popen(["blender", "-P", "tests/resources/scripts/main.py", "-b"])
+        # TODO: Figure out a more intelligent way to wait for Blender to load.
+        sleep(30)
+        assert discover_nodes() != []
+        # TODO: Figure out a mulit-platform way to force-kill Blender
+        print("Don't forget to kill blender")
 
 
 def test_get_info():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as socket_obj:
         socket_obj.bind(("localhost", 8080))
         socket_obj.listen()
-#        socket_thread = Thread(target=helper_thread, args=(socket_obj,))
-#        socket_thread.start()
         assert get_info("localhost") == {}
