153fc7cb0f55f6c115248871259328be3c555bb2
Radek Golan jr. (golyalpha) <golyalpha@gmail.com>
2018-09-22 07:51:24 +0200

    Created entrypoint.

--- blender_renderfarm/__main__.py
+++ blender_renderfarm/__main__.py
@@ -1,3 +1,28 @@
 from server_discovery import discover_nodes
+from send_blend import send_to_nodes
+from collector import collect_frames
+from encoder import encode_frames
+from utils import get_info
+from tempfile import TemporaryDirectory
+from tkinter import filedialog
+from tkinter import Tk
 
-print(discover_nodes())
+Tk().withdraw()
+
+blendfile = filedialog.askopenfilename()
+outfile = filedialog.asksaveasfilename()
+
+print("Discovering nodes.")
+nodes = discover_nodes()
+print("Found:")
+for node in nodes: print(node)
+print()
+print("Sending blendfile to nodes.")
+send_to_nodes(nodes, blendfile)
+info = get_info(nodes[0])
+with TemporaryDirectory() as renderDir:
+    print("Collecting frames.")
+    collect_frames(nodes, renderDir)
+    print("Encoding frames.")
+    encode_frames(renderDir, outfile, info["rate"])
+    print("Done!")
