7ff9c28230eafab24b3670a88a6ca780d3a64c85
Radek Golan jr. (golyalpha) <golyalpha@gmail.com>
2018-09-22 07:51:02 +0200

    Updated the render script.

--- blender_renderfarm/blender_scripts/main.py
+++ blender_renderfarm/blender_scripts/main.py
@@ -8,32 +8,32 @@ from bpy.app.handlers import persistent
 
 @persistent
 def handle_render():
-    print("Render finished, waiting for a new file.")    
-    with NamedTemporaryFile(delete=False, suffix=".blend") as datafile:
-        waiting = True
-        while waiting:
-            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-                s.bind(("0.0.0.0", 3558))
-                s.listen(0)
-                conn, addr = s.accept()
-                with conn:
-                    print("Receiving blend file from {}".format(addr))
-                    while True:
-                        data = conn.recv(1024)
-                        if data == b"PING":
-                            conn.sendall(b"PONG")
-                            break
-                        if data == b"STOP":
-                            conn.sendall(b"ACK")
-                            exit(0)
-                        if not data:
-                            waiting = False
-                            break
-                        datafile.write(data)
-    bpy.ops.wm.open_mainfile(filepath=datafile.name)
-    bpy.data.scenes['Scene'].render.image_settings.file_format = 'FRAMESERVER'
-    bpy.ops.render.render(animation=True)
-    os.remove(datafile.name)
+    while True:  
+        with NamedTemporaryFile(delete=False, suffix=".blend") as datafile:
+            waiting = True
+            while waiting:
+                with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+                    s.bind(("0.0.0.0", 3558))
+                    s.listen(0)
+                    conn, addr = s.accept()
+                    with conn:
+                        print("Receiving blend file from {}".format(addr))
+                        while True:
+                            data = conn.recv(1024)
+                            if data == b"PING":
+                                conn.sendall(b"PONG")
+                                break
+                            if data == b"STOP":
+                                conn.sendall(b"ACK")
+                                exit(0)
+                            if not data:
+                                waiting = False
+                                break
+                            datafile.write(data)
+        bpy.ops.wm.open_mainfile(filepath=datafile.name)
+        bpy.data.scenes['Scene'].render.image_settings.file_format = 'FRAMESERVER'
+        bpy.ops.render.render(animation=True)
+        os.remove(datafile.name)
+        print("Render finished, waiting for a new file.")  
 
-bpy.app.handlers.render_complete.append(handle_render)
 handle_render()
