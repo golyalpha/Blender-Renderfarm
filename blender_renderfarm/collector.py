1496812a513eeb8f7e3c0444835d1521d7d9b989
Radek Golan jr. (golyalpha) <golyalpha@gmail.com>
2018-09-22 07:49:46 +0200

    Collector updates.

--- blender_renderfarm/collector.py
+++ blender_renderfarm/collector.py
@@ -5,15 +5,30 @@ import logging
 from json import dumps, loads
 from os import rmdir
 from os.path import join as pjoin
-from tempfile import mkdtemp
 from threading import Thread
-
 import requests
 
-from blender_renderfarm.utils import get_info
+from utils import get_info
 
 
-def render_collector(node, start, end, step, offset, directory):
-    for frame in range(start+offset, end+offset, step):
-        with open(pjoin(directory, f"{frame}.ppm"), "w") as framefile:
-            framefile.write(requests.get(f"http://{node}:8080/images/ppm/{frame}.ppm"))
+def frame_collector(node:str, start:int, end:int, step:int, offset:int, directory:str):
+    for frame in range(start+offset, end+offset+1, step):
+        with open(pjoin(directory, "%03d.ppm" % frame), "wb") as framefile:
+            framefile.write(requests.get(f"http://{node}:8080/images/ppm/{frame}.ppm").content)
+    try:
+        requests.get(f"http://{node}:8080/close.txt")
+    except ConnectionError:
+        pass
+
+
+def collect_frames(nodes:list, directory:str):
+    render_info = get_info(nodes[0])
+    threads = [Thread(target=frame_collector, args=(
+        node, 
+        render_info["start"],
+        render_info["end"],
+        len(nodes),
+        nodes.index(node),
+        directory)) for node in set(nodes)]
+    for thread in threads: thread.start()
+    for thread in threads: thread.join()
